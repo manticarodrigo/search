52cd3823507bac3d3e8ab87f493c2ac9392551fd
Your Name <manticarodrigo@gmail.com>
2023-08-18 01:40:08 -0600

    Fix bug and runtime

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,13 @@
+import React from "react"
+
 import "./globals.css"
 
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 
 const inter = Inter({ subsets: ["latin"] })
+
+export const runtime = "edge"
 
 export const metadata: Metadata = {
   title: "Synopsis",
