6f75f7d1bf45e200cd446c0deb4c5329d082451a
Your Name <manticarodrigo@gmail.com>
2023-08-18 12:00:17 -0600

    Logo

--- src/components/nav/header.tsx
+++ src/components/nav/header.tsx
@@ -1,10 +1,17 @@
+import Image from "next/image"
+
 import { ModeToggle } from "./mode-toggle"
 
 export function Header() {
   return (
-    <header className="flex w-full items-center justify-between border-b p-2">
-      <h1 className="text-lg font-light">Synopsis</h1>
-      <ModeToggle />
+    <header className="w-full border-b">
+      <div className="container flex w-full items-center justify-between p-2">
+        <div className="flex items-center gap-2">
+          <Image src="/bulb.svg" alt="logo" width={20} height={20} />
+          <h1 className="font-mono text-lg font-bold lowercase">Synopsis</h1>
+        </div>
+        <ModeToggle />
+      </div>
     </header>
   )
 }
