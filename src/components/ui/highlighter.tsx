8cf1485ebde5c32c8655b05c19db8196080e4676
Your Name <manticarodrigo@gmail.com>
2023-08-19 23:02:36 -0600

    Fix highlighter

--- src/components/ui/highlighter.tsx
+++ src/components/ui/highlighter.tsx
@@ -1,36 +1,43 @@
-const Highlighter = ({ children: text }: { children: string }) => {
+function Highlighter({ children: text }: { children: string }) {
   // Define the range of Unicode characters.
   const startMarker = "\uE000"
   const endMarker = "\uE001"
 
   // Create a function to break the string into parts.
   const getHighlightedParts = (str: string) => {
-    const parts = []
-    let currentIndex = 0
-    while (currentIndex < str.length) {
-      const startIndex = str.indexOf(startMarker, currentIndex)
-      if (startIndex === -1) {
-        parts.push({ text: str.substring(currentIndex), isHighlighted: false })
-        break
-      } else {
-        if (startIndex > currentIndex) {
+    try {
+      const parts = []
+      let currentIndex = 0
+      while (currentIndex < str.length) {
+        const startIndex = str.indexOf(startMarker, currentIndex)
+        if (startIndex === -1) {
           parts.push({
-            text: str.substring(currentIndex, startIndex),
+            text: str.substring(currentIndex),
             isHighlighted: false,
           })
+          break
+        } else {
+          if (startIndex > currentIndex) {
+            parts.push({
+              text: str.substring(currentIndex, startIndex),
+              isHighlighted: false,
+            })
+          }
+          const endIndex = str.indexOf(endMarker, startIndex)
+          if (endIndex === -1) {
+            throw new Error("Mismatched markers")
+          }
+          parts.push({
+            text: str.substring(startIndex + startMarker.length, endIndex),
+            isHighlighted: true,
+          })
+          currentIndex = endIndex + endMarker.length
         }
-        const endIndex = str.indexOf(endMarker, startIndex)
-        if (endIndex === -1) {
-          throw new Error("Mismatched markers")
-        }
-        parts.push({
-          text: str.substring(startIndex + startMarker.length, endIndex),
-          isHighlighted: true,
-        })
-        currentIndex = endIndex + endMarker.length
       }
+      return parts
+    } catch {
+      return [{ text: str, isHighlighted: false }]
     }
-    return parts
   }
 
   // Render the parts.
